d1d6630c5d756fbc98dce54f9c9a8e04530aca7d
hanyohades <hanyohades0@gmail.com>
2025-03-04 01:42:05 -0800

    use mapping method for variables in changeLanguage function

--- script.js
+++ script.js
@@ -25,46 +25,53 @@ function videoScroll() {
 
 function changeLanguage() {
   const button = document.querySelector("#changeLanguageBtn");
-  const aboutText = document.querySelector("#aboutText");
-  const apptHeaderText = document.querySelector("#apptHeaderText");
-  const contactText = document.querySelector("#contactText");
-  const navContactText = document.querySelector("#navContactText");
-  const locationText = document.querySelector("#locationText");
-  const priceHeaderTxt = document.querySelector("#priceHeaderText");
-  const hoursText = document.querySelector("#hoursText");
-  const timeText = document.querySelector("#timeText");
-  const sloganText = document.querySelector("#sloganText");
-  const artWorksBtnTxt = document.querySelector("#see-artworks");
+  
+  const languageContent = {
+    VN: {
+      aboutText: "Giới thiệu",
+      apptHeaderText: "Đặt lịch",
+      apptText: "Liên hệ trực tiếp để đặt lịch hẹn.",
+      artWorksBtnTxt: "Xem Thêm",
+      navContactText: "Liên hệ",
+      contactText: "Liên hệ",
+      locationText: "Địa chỉ",
+      hoursText: "Giờ làm việc",
+      timeText: "Hằng ngày: 10 giờ sáng - 7 giờ tối",
+      priceHeaderTxt: "Bảng giá",
+      pText: "100 ngàn cho 1 giờ",
+      sloganText: "9th.studio một trong những tattoo shops đỉnh nhất đất Đà Thành."
+    },
+    Eng: {
+      aboutText: "About",
+      apptHeaderText: "Appointments",
+      apptText: "Contact for booking!",
+      artWorksBtnTxt: "Check out all artworks",
+      navContactText: "Contact",
+      contactText: "Contact",
+      locationText: "Location",
+      hoursText: "Open Hours",
+      timeText: "Everyday: 10AM - 7PM",
+      priceHeaderTxt: "Pricings",
+      pText: "$100 for an hour",
+      sloganText: "9th.studio is proud to be one of the best tattoo shops in Da Nang, Vietnam."
+    }
+  };
 
   button.innerHTML = button.innerHTML === "VN" ? "Eng" : "VN";
 
-  if (button.innerHTML !== "VN") {
-    aboutText.innerHTML = "Giới thiệu";
-    apptHeaderText.innerHTML = "Đặt lịch";
-    apptText.innerHTML = "Liên hệ trực tiếp để đặt lịch hẹn."
-    artWorksBtnTxt.innerHTML = "Xem Thêm";
-    navContactText.innerHTML = "Liên hệ";
-    contactText.innerHTML = "Liên hệ";
-    locationText.innerHTML = "Địa chỉ";
-    hoursText.innerHTML = "Giờ làm việc";
-    timeText.innerHTML = "Hằng ngày: 10 giờ sáng - 7 giờ tối";
-    priceHeaderTxt.innerHTML = "Bảng giá";
-    pText.innerHTML = "100 ngàn cho 1 giờ";
-    sloganText.innerHTML = "9th.studio một trong những tattoo shops đỉnh nhất đất Đà Thành.";
-  }
-  
-  else {
-    aboutText.innerHTML = "About";
-    apptHeaderText.innerHTML = "Appointments";
-    apptText.innerHTML = "Contact for booking!"
-    artWorksBtnTxt.innerHTML = "Check out all artworks";
-    navContactText.innerHTML = "Contact";
-    contactText.innerHTML = "Contact";
-    locationText.innerHTML = "Location";
-    hoursText.innerHTML = "Open Hours";
-    timeText.innerHTML = "Everyday: 10AM - 7PM";
-    priceHeaderTxt.innerHTML = "Pricings";
-    pText.innerHTML = "$100 for an hour";
-    sloganText.innerHTML = "9th.studio is proud to be one of the best tattoo shops in Da Nang, Vietnam.";
-  }
+  var displayLanguage = button.innerHTML === "VN" ? "Eng" : "VN";
+
+  const content = languageContent[displayLanguage];
+  document.querySelector("#aboutText").innerHTML = content.aboutText;
+  document.querySelector("#apptHeaderText").innerHTML = content.apptHeaderText;
+  document.querySelector("#apptText").innerHTML = content.apptText;
+  document.querySelector("#see-artworks").innerHTML = content.artWorksBtnTxt;
+  document.querySelector("#navContactText").innerHTML = content.navContactText;
+  document.querySelector("#contactText").innerHTML = content.contactText;
+  document.querySelector("#locationText").innerHTML = content.locationText;
+  document.querySelector("#hoursText").innerHTML = content.hoursText;
+  document.querySelector("#timeText").innerHTML = content.timeText;
+  document.querySelector("#priceHeaderText").innerHTML = content.priceHeaderTxt;
+  document.querySelector("#pText").innerHTML = content.pText;
+  document.querySelector("#sloganText").innerHTML = content.sloganText;
 }
