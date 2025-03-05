b657aa83df8b651fd119692bbee68f837eaf5750
Bao Than <bao.v.than@gmail.com>
2025-03-04 16:56:04 -0800

    hot-fix-mobile-ratio-for-mobile-device-about-section

--- script.js
+++ script.js
@@ -25,16 +25,26 @@ function initCarousel() {
       // when window width is <= 320px
       320: {
         slidesPerView: 1,
-        spaceBetween: 5
+        spaceBetween: 10
       },
       // when window width is <= 480px
-      425: {
+      480: {
         slidesPerView: 2,
         spaceBetween: 10
       },
-      // when window width is <= 640px
+      // when window width is <= 768px
       768: {
         slidesPerView: 3,
+        spaceBetween: 15
+      },
+      // when window width is <= 992px
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      },
+      // when window width is > 992px
+      1200: {
+        slidesPerView: 5,
         spaceBetween: 20
       }
     }
