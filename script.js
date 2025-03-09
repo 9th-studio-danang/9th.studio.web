0e97292b5f39f95394f84d32d74ce5402c69e433
Luan Truong <truongluan303@gmail.com>
2025-03-09 03:20:00 -0700

    Code clean up and some improvements to the UI

--- script.js
+++ script.js
@@ -13,43 +13,47 @@ $(document).ready(function() {
   var active2 = false;
   var active3 = false;
 
-    $('.social-chat-container').on('click', function() {
-    
-      if (!active1){
-        $(this).find('.facebook-box').css({
-          'background-color': '#0099FF',
-          'transform': 'translate(0px,-125px)',
-        });
-      } else{
-        $(this).find('.facebook-box').css({
-          'background-color': 'gray', 
-          'transform': 'none'});
-      } 
+  $('.social-chat-container').on('click', function() {
+    if (!active1){
+      $(this).find('.facebook-box').css({
+        'background-color': '#0099FF',
+        'transform': 'translate(0px, -125px)',
+      });
+    } else{
+      $(this).find('.facebook-box').css({
+        'background-color': 'gray',
+        'transform': 'none'
+      });
+    }
 
-      if (!active2){
-        $(this).find('.instagram-box').css({
-          'background': "linear-gradient(45deg, #f9ce34, #ee2a7b, #6228d7)",
-          'transform': 'translate(-78px,-85px)'});
-      } else{
-        $(this).find('.instagram-box').css({
-          'background-color': 'gray', 
-          'transform': 'none'});
-      } 
+    if (!active2){
+      $(this).find('.instagram-box').css({
+        'background': 'linear-gradient(45deg, #f9ce34, #ee2a7b, #6228d7)',
+        'transform': 'translate(-78px, -85px)'
+      });
+    } else{
+      $(this).find('.instagram-box').css({
+        'background-color': 'gray',
+        'transform': 'none'
+      });
+    }
 
-      if (!active3){
-        $(this).find('.zalo-box').css({
-          'background-color': '#0168fe', 
-          'transform': 'translate(-120px,0px)'});
-      } else{
-        $(this).find('.zalo-box').css({
-          'background-color': 'gray', 
-          'transform': 'none'});
-      } 
+    if (!active3){
+      $(this).find('.zalo-box').css({
+        'background-color': '#0168fe',
+        'transform': 'translate(-120px, 0px)'
+      });
+    } else{
+      $(this).find('.zalo-box').css({
+        'background-color': 'gray',
+        'transform': 'none'
+      });
+    }
 
-      active1 = !active1;
-      active2 = !active2;
-      active3 = !active3;
-    });
+    active1 = !active1;
+    active2 = !active2;
+    active3 = !active3;
+  });
 });
 
 // Initialize the carousel gallery
