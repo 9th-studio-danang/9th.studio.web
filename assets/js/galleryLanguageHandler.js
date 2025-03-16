8f0028284e8a26e69a41633d4aa188a9e32d0f97
Luan Truong <83048295+truongluan303@users.noreply.github.com>
2025-03-15 20:49:56 -0700

    Update content for gallery page (#9)

---
+++ assets/js/galleryLanguageHandler.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  var langChangeButton = document.querySelector("#change-lang-btn");
+  langChangeButton.innerHTML = "VN";
+});
+
+function changeLanguage() {
+    const button = document.querySelector("#change-lang-btn");
+
+    const languageContent = {
+        VN: {
+          aboutText: "Giới thiệu",
+          navContactText: "Liên hệ",
+        },
+        Eng: {
+          aboutText: "About",
+          navContactText: "Contact",
+        }
+      };
+
+      button.innerHTML = button.innerHTML === "VN" ? "Eng" : "VN";
+      var displayLanguage = button.innerHTML === "VN" ? "Eng" : "VN";
+
+      const content = languageContent[displayLanguage];
+      document.querySelector("#aboutText").innerHTML = content.aboutText;
+      document.querySelector("#navContactText").innerHTML = content.navContactText;
+}
