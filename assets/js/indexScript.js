465f1233c085770a9da57e88e32c701a31bc5885
Luan Truong <truongluan303@gmail.com>
2025-04-19 23:37:09 -0700

    Some code cleanup and refactoring

--- assets/js/indexScript.js
+++ assets/js/indexScript.js
@@ -31,14 +31,53 @@ import { artistsData } from './data/artists.js';
 
 
 window.addEventListener('load', function() {
-  videoScroll();
   initCarousel();
+  hideLoader();
+  pausePlayIntroVideo(false); // play intro video
 });
-window.addEventListener('scroll', videoScroll);
 
-// Function to transform the social chat buttons
+window.addEventListener('scroll', function(){
+  videoScroll();
+});
+
 $(document).ready(function() {
+  pausePlayIntroVideo(true); // pause intro video until page content is fully loaded
+  transformSocialChatButtons();
+});
 
+document.addEventListener("DOMContentLoaded", () => {
+  injectImagesToCarouselGallery();
+  injectArtists();
+});
+
+/* Hide the loader when the page is loaded */
+function hideLoader() {
+  const preloader = document.getElementById("preloader");
+  if (!preloader) {
+      return;
+  }
+  preloader.style.opacity = "0";
+  preloader.style.pointerEvents = "none";
+  setTimeout(() => preloader.remove(), 700);
+
+  if (document.body.classList.contains('no-scroll')) {
+      document.body.classList.remove('no-scroll');
+  }
+};
+
+function pausePlayIntroVideo(isPause) {
+  const introVid = document.getElementById('intro-tattoo-show-video');
+  if (isPause) {
+    introVid.pause();
+  } else {
+    introVid.play();
+  }
+}
+
+/**
+ * Function to transform the social chat buttons
+ */
+function transformSocialChatButtons() {
   // activeX = true ==> EXPAND
   // activeX = false ==> COLLAPSE
   var active1 = false;
@@ -86,7 +125,7 @@ $(document).ready(function() {
     active2 = !active2;
     active3 = !active3;
   });
-});
+};
 
 // Initialize the carousel gallery
 function initCarousel() {
@@ -249,8 +288,3 @@ function injectArtists() {
     container.appendChild(div);
   });
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-  injectImagesToCarouselGallery();
-  injectArtists();
-});
